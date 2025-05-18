a2d6b90f69b6f115131eb93dd8cf0ac6fa516620
Noah.TaeHwan <noah.taehwan@gmail.com>
2025-05-18 12:58:14 +0900

    init

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '~/common/components/ui/button';
+
+export default function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Button>hello</Button>
+    </div>
+  );
+}
